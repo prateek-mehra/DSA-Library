9756bdc7374561e05f9e317d0bc2ebb9d8747592
prateek-mehra <63323034+prateek-mehra@users.noreply.github.com>
2021-08-31 11:25:30 +0530

    added counting and shifting bits concept

--- bit-manipulation/Counting and shifting set bits (1368D).cpp
+++ bit-manipulation/Counting and shifting set bits (1368D).cpp
@@ -11,9 +11,9 @@ using namespace std;
 #define all(a) a.begin(), a.end()
 #define endl '\n'
 #define boost                    \
-    ios::sync_with_stdio(false); \
-    cin.tie(0);                  \
-    cout.tie(0);
+	ios::sync_with_stdio(false); \
+	cin.tie(0);                  \
+	cout.tie(0);
 #define mod 1000000007
 #define inf 1e18
 #define pf push_front
@@ -31,38 +31,38 @@ typedef vector<vi> vvi;
 
 int32_t main()
 {
-    // boost
-    int t=1;
-   // cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-        int cnt[20]={0};
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<20;j++)
-            {
-                if((a[i]&(1<<j)))
-                cnt[j]++;
-            }
-        }
-        int ans=0;
-        for(int i=0;i<n;i++)
-        {
-            int temp=0;
-            for(int j=0;j<20;j++)
-            {
-                if(i<cnt[j])temp|=(1<<j);
-            }
-            ans+=temp*temp;
-        }
-        cout<<ans<<endl;
-
-    }
+	// boost
+	int t = 1;
+	// cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int a[n];
+		for (int i = 0; i < n; i++)
+		{
+			cin >> a[i];
+		}
+		int cnt[20] = {0};
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < 20; j++)
+			{
+				if ((a[i] & (1 << j)))
+					cnt[j]++;
+			}
+		}
+		int ans = 0;
+		for (int i = 0; i < n; i++)
+		{
+			int temp = 0;
+			for (int j = 0; j < 20; j++)
+			{
+				if (i < cnt[j])
+					temp |= (1 << j);
+			}
+			ans += temp * temp;
+		}
+		cout << ans << endl;
+	}
 }
